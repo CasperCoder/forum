c4daa37b01825d8f8762f1e95628e2214ffb1482
Casper Coder <zagrodzinski@gmail.com>
2024-11-14 23:28:51 +0100

    Forum Application

---
+++ lib/forum_web/controllers/page_controller.ex
@@ -0,0 +1,20 @@
+defmodule ForumWeb.PageController do
+  use ForumWeb, :controller
+
+  def home(conn, _params) do
+    # The home page is often custom made,
+    # so skip the default app layout.
+    render(conn, :home, layout: false)
+  end
+
+  def users(conn, _params)do
+    users = [
+
+      %{id: 1, name: "Alice", email: "alice@email.com"},
+      %{id: 2, name: "Bob", email: "bob@email.com"}
+    ]
+    IO.puts("Users function hit!")
+ # render(conn, :users, users: users, layout: false)
+  json(conn, %{users: users})
+end
+end
